031d1efe117752d85602a2cae606ce72ee58d29f
g11947sa <g11947sa@s30420.tsuda.ac.jp>
2014-04-11 11:35:20 +0900

    日本語を追加しました。

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -15,5 +15,6 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        System.out.println("こんにちは世界！こんにちは！私は元気です！こんにちは！");
     }
 }
